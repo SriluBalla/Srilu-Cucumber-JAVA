b1402e9cee4093c9f700d7ea28c7b335fe00cec1
sriluballa <SriluNBalla@Gmail.com>
2024-01-31 16:38:14 -0500

    Adding Helpers, Features, Steps

---
+++ src/test/java/helpers/envHelper.java
@@ -0,0 +1,34 @@
+package helpers;
+
+public class envHelper {
+
+    private static String env;
+    private static String portal;
+
+    public static String getBaseURL(String path) throws Throwable {
+        return chooseURL(path);
+    }
+
+    public static String chooseEnv(){
+
+        env = System.getProperty("ENV");
+
+        if (env == null){
+            env = System.getenv("ENV");
+            if (env == null){
+                env = "dev";
+            }
+        }
+        return env;
+    }
+
+    private static String chooseURL(String path) {
+        switch (chooseEnv())
+        {
+            case "dev":    env = "www.google.com" + path; break;
+            case "uat":     env = "https://courses.ultimateuat.com" + path;    break;
+            case "qa":      env = "https://courses.ultimateqa.com" + path;
+        }
+        return env;
+    }
+}
